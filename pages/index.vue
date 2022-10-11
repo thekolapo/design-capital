aa035d7181401dc04c3863e349e6cece727283ab
Kolapo Oni <kolapo.oni@flutterwavego.com>
2022-10-11 16:15:03 +0100

    setup base project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
