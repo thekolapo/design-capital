2546220ca5522f7607d1db5a967dd59a7ddbd9a5
Kolapo Oni <kolapo.oni@flutterwavego.com>
2022-10-11 17:10:28 +0100

    abstract typography styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div class="home">
+    <div class="home__hero">
+      <h1 class="u-font-h1">Get backed by senior designers around the world</h1>
+      <button class="c-button c-button--secondary">
+        Connect with a SENIOR designer
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
