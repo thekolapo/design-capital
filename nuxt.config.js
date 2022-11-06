0fcd71be3ceefc469cd2d1fe129c0ca14ff44812
Kolapo Oni <onikolapo@gmail.com>
2022-11-06 21:19:16 +0100

    delete unused plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vue-paginate', mode: 'client' }],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- plugins/vue-paginate.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Paginate from 'vuejs-paginate'
-
-Vue.component('Paginate', Paginate)
